550f55c20e25982e00a7f6852a7cc6a866d30678
Abhishek <sutrave.abhishek@gmail.com>
2014-06-24 22:26:08 -0700

    error deleting myPixel. also examine for loop (currently commented out)

--- lab_debug/main.cpp
+++ lab_debug/main.cpp
@@ -1,74 +1,79 @@
 #include <cstdlib>
 #include "png.h"
-
+#include <iostream>
 // sets up the output image
 PNG * setupOutput(int w, int h);
-
+using namespace std;
 // Returns my favorite color
 RGBAPixel * myFavoriteColor(int intensity);
 
 int main()
 {
-	// Load in.png
-	PNG * original;
+    // Load in.png
+    PNG origPNG = PNG("in.png");
+    PNG * original = &origPNG;
 
-	original->readFromFile("in.png");
-	int width  = original->width();
-	int height = original->height();
+//    original->readFromFile("in.png");
 
-	// Create out.png
-	PNG * output;
-	setupOutput(width, height);	
+    int width  = original->width();
 
-	// Loud our favorite color to color the outline
-	RGBAPixel * myPixel = myFavoriteColor(192);
+    int height = original->height();
 
-	// Go over the whole image, and if a pixel differs from that to its upper
-	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
-	{
-		for (int x = 1; 0 < x < width; x++)
-		{
-			// Calculate the pixel difference
-			RGBAPixel * prev = (*original)(x-1, y-1);
-			RGBAPixel * curr = (*original)(x  , y  );
-			int diff = abs(curr->red   - prev->red  ) +
-					   abs(curr->green - prev->green) +
-					   abs(curr->blue  - prev->blue );
+    // Create out.png
+    PNG * output = setupOutput(width, height);	
 
-			// If the pixel is an edge pixel,
-			// color the output pixel with my favorite color
-			RGBAPixel * currOutPixel = (*output)(x,y);
-			if (diff > 100)
-				currOutPixel = myPixel;
-		}
-	}
-	
-	// Save the output file
-	output->writeToFile("out.png");
+    // Loud our favorite color to color the outline
+    RGBAPixel * myPixel = myFavoriteColor(192);
 
-	// Clean up memory
-	delete myPixel;
-	delete output;
-	delete original;
-	return 0;
+    // Go over the whole image, and if a pixel differs from that to its upper
+    // left, color it my favorite color in the output
+    for (int y = 1; 0 < y && y < height; y++)
+    {        
+        for (int x = 1; 0 < x && x < width; x++)
+        {
+            // Calculate the pixel difference
+//            RGBAPixel * prev = (*original)(x-1, y-1);
+//            RGBAPixel * curr = (*original)(x  , y  );
+            // int diff = abs(curr->red   - prev->red  ) +
+            //     abs(curr->green - prev->green) +
+            //             abs(curr->blue  - prev->blue );
+
+            // // If the pixel is an edge pixel,
+            // // color the output pixel with my favorite color
+            // RGBAPixel * currOutPixel = (*output)(x,y);
+            // if (diff > 100)
+            //     currOutPixel = myPixel;
+        }
+    }
+    cout << "reached line "<< __LINE__<<endl;
+    // Save the output file
+    output->writeToFile("out.png");
+    cout << "reached line "<< __LINE__<<endl;    
+    // Clean up memory
+    delete myPixel;
+    cout << "reached line "<< __LINE__<<endl;    
+    delete output;
+    cout << "reached line "<< __LINE__<<endl;    
+    delete original;
+    cout << "reached line "<< __LINE__<<endl;    
+    return 0;
 }
 
 
 // sets up the output image
 PNG * setupOutput(int w, int h)
 {
-	PNG * image = new PNG(w, h);
-	return image;
+    PNG * image = new PNG(w, h);
+    return image;
 }
 
 
 // Returns my favorite color
 RGBAPixel * myFavoriteColor(int intensity)
 {
-	RGBAPixel color;
-	color.red   = 0;
-	color.green = intensity/2;
-	color.blue  = intensity;
-	return &color;
+    RGBAPixel color;
+    color.red   = 0;
+    color.green = intensity/2;
+    color.blue  = intensity;
+    return &color;
 }
